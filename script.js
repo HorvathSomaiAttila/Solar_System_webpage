522d3a1c3f90f03caece60c8d6c79ac0ecde41b9
HorvathSomaiAttila <horvathsomaiattila@gmail.com>
2024-12-05 15:41:26 +0100

    bolygók mozgása

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+
 document.addEventListener("scroll", () => {
     const sections = document.querySelectorAll(".planet .content");
     sections.forEach((section) => {
         const rect = section.getBoundingClientRect();
         if (rect.top >= 0 && rect.top <= window.innerHeight * 0.5) {
             section.classList.add("active");
+            section.querySelector(".picture").classList.add("active");
+            section.querySelector(".text").classList.add("active");
         } else {
             section.classList.remove("active");
+            section.querySelector(".picture").classList.remove("active");
+            section.querySelector(".text").classList.remove("active");
         }
     });
 });
