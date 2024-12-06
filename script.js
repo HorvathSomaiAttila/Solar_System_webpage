8e85977a5fd1af76a27d268c0c64729885c1577c
HorvathSomaiAttila2006 <horvathsomaiattila@gmail.com>
2024-12-06 11:41:43 +0100

    formázás

--- script.js
+++ script.js
@@ -1,41 +1,24 @@
 function debounce(func, wait) {
-
     let timeout;
-
     return function (...args) {
-
         const context = this;
-
         clearTimeout(timeout);
-
         timeout = setTimeout(() => func.apply(context, args), wait);
-
     };
-
 }
 
 
 
 document.addEventListener("scroll", debounce(() => {
-
     const sections = document.querySelectorAll(".planet .content");
-
     sections.forEach((section) => {
-
         const rect = section.getBoundingClientRect();
-
         if (rect.top >= 0 && rect.top <= window.innerHeight * 0.5) {
-
             section.classList.add("active");
-
         } else {
-
             section.classList.remove("active");
-
         }
-
     });
-
 }, 50));
 
 
